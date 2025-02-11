48f8b82a88ae6c63a72fae8662ffc64e06ec76dd
Matthew Bradbury <79022571+DESTROYERMJB@users.noreply.github.com>
2025-02-11 17:47:12 -0600

    changes to skills route

--- 24-25/85B_HIGHSTAKES skills/src/main.cpp
+++ 24-25/85B_HIGHSTAKES skills/src/main.cpp
@@ -1,12 +1,26 @@
+
+
 #include  "main.h"
 #include "lemlib/api.hpp" // IWYU pragma: keep
+#include "pros/llemu.hpp"
 ASSET(skills_txt);
+ASSET(skills1_txt);
+ASSET(skills2_txt);
+ASSET(skills3_txt);
+ASSET(skills4_txt);
+ASSET(skills5_txt);
+ASSET(skills6_txt);
+ASSET(skills7_txt);
+ASSET(skills8_txt);
+ASSET(skills9_txt);
+ASSET(skills10_txt);
+ASSET(skills11_txt);
 pros::Controller controller(pros::E_CONTROLLER_MASTER);
 pros::MotorGroup leftMotors({-15,-19,-17},pros::MotorGearset::blue);
 pros::MotorGroup rightMotors({9,10,8},pros::MotorGearset::blue);
 lemlib::Drivetrain drivetrain(&leftMotors,
                               &rightMotors,
-                              12,
+                              16,
                               lemlib::Omniwheel::NEW_325,
                               360,
                               2);
@@ -14,10 +28,10 @@ pros::Motor intake(1,pros::MotorGearset::green);
 pros::Motor conveyor(-12,pros::MotorGearset::red);
 //odom parts
 pros::Imu imu(13);
-pros::Rotation horizontalEnc(-3);
-pros::Rotation verticalEnc(-4);
-lemlib::TrackingWheel horizontal(&horizontalEnc, lemlib::Omniwheel::NEW_325, 0);
-lemlib::TrackingWheel vertical(&verticalEnc, lemlib::Omniwheel::NEW_325, 0);
+pros::Rotation horizontalEnc(2);
+pros::Rotation verticalEnc(14);
+lemlib::TrackingWheel horizontal(&horizontalEnc, lemlib::Omniwheel::NEW_325, -0.125);
+lemlib::TrackingWheel vertical(&verticalEnc, lemlib::Omniwheel::NEW_325, -1.625);
 //odometry setting
 lemlib::OdomSensors sensors(&vertical, // vertical tracking wheel 1, set to null
                             nullptr, // vertical tracking wheel 2, set to nullptr as we are using IMEs
@@ -27,26 +41,26 @@ lemlib::OdomSensors sensors(&vertical, // vertical tracking wheel 1, set to null
 );
 
 // lateral PID controller
-lemlib::ControllerSettings lateral_controller(10, // proportional gain (kP)
+lemlib::ControllerSettings lateral_controller(15, // proportional gain (kP)
                                               0, // integral gain (kI)
-                                              3, // derivative gain (kD)
-                                              0, // anti windup
-                                              0, // small error range, in inches
-                                              0, // small error range timeout, in milliseconds
-                                              0, // large error range, in inches
-                                              0, // large error range timeout, in milliseconds
-                                              0 // maximum acceleration (slew)
+                                              7, // derivative gain (kD)
+                                              3, // anti windup
+                                              1, // small error range, in inches
+                                              100, // small error range timeout, in milliseconds
+                                              3, // large error range, in inches
+                                              500, // large error range timeout, in milliseconds
+                                              10 // maximum acceleration (slew)
 );
 
 // angular PID controller
-lemlib::ControllerSettings angular_controller(2, // proportional gain (kP)
+lemlib::ControllerSettings angular_controller(7, // proportional gain (kP)
                                               0, // integral gain (kI)
-                                              10, // derivative gain (kD)
-                                              0, // anti windup
-                                              0, // small error range, in degrees
-                                              0, // small error range timeout, in milliseconds
-                                              0, // large error range, in degrees
-                                              0, // large error range timeout, in milliseconds
+                                              60, // derivative gain (kD)
+                                              3, // anti windup
+                                              1, // small error range, in inches
+                                              100, // small error range timeout, in milliseconds
+                                              3, // large error range, in inches
+                                              500, // large error range timeout, in milliseconds
                                               0 // maximum acceleration (slew)
 );
 
@@ -55,7 +69,7 @@ lemlib::Chassis chassis(drivetrain, // drivetrain settings
                         angular_controller, // angular PID settings
                         sensors // odometry sensors
 );
-int clampState = 0;
+int clampState = 1;
 int rolling = 0;
 
 void ring(){
@@ -71,16 +85,24 @@ void ring(){
             conveyor.move(-90);
             rolling=1;
             pros::delay(500);
-        } else if(controller.get_digital(pros::E_CONTROLLER_DIGITAL_R1)==1 or controller.get_digital(pros::E_CONTROLLER_DIGITAL_L1)==1 and rolling ==1){
+        } else if(controller.get_digital(pros::E_CONTROLLER_DIGITAL_L1)==1 and rolling ==1){
             intake.move(0);
             conveyor.move(0);
             rolling=0;
             pros::delay(500);
+        } else if(controller.get_digital(pros::E_CONTROLLER_DIGITAL_L2)==1 and rolling ==1){
+            intake.move(-127);
+            conveyor.move(-90);
+            pros::delay(500);
+            intake.move(127);
+            conveyor.move(127);
+        }else {
+            pros::delay(25);
         }
     }
 }
 
-
+//auton selector
 /**
  * Runs initialization code. This occurs as soon as the program is started.
  *
@@ -122,38 +144,29 @@ void competition_initialize() {
  * the Field Management System or the VEX Competition Switch in the autonomous
  * mode. Alternatively, this function may be called in initialize or opcontrol
  * for non-competition testing purposes.
- *
  * If the robot is disabled or communications is lost, the autonomous task
  * will be stopped. Re-enabling the robot will restart the task, not re-start it
  * from where it left off.
  */
 void autonomous() {
     pros::ADIPneumatics clamp('a',false,false);
-    chassis.setPose(0, 0, 0);
-    // turn to face heading 90 with a very long timeout
-    //chassis.turnToHeading(90, 100000);
+    chassis.setPose(-58.092,17.826,240);
+    chassis.follow(skills_txt,10,2000,false,false);
+    clamp.extend();
+    pros::delay(200);
     intake.move(127);
     conveyor.move(127);
-    leftMotors.set_encoder_units_all(MOTOR_ENCODER_DEGREES);
-    rightMotors.set_encoder_units_all(MOTOR_ENCODER_DEGREES);
+    pros::delay(200);
+    chassis.turnToHeading(90,1000,{},false);
+    chassis.follow(skills1_txt,5,2000,true,false);
+    chassis.turnToHeading(0,1000,{},false);
+    chassis.follow(skills2_txt,5,10000,true,false);
     pros::delay(500);
-    leftMotors.move_relative(1080,50);
-    rightMotors.move_relative(1080,50);
-    while(not imu.get_heading()<=270){
-        leftMotors.move(-40);
-        rightMotors.move(40);
-    }
-    leftMotors.brake();
-    rightMotors.brake();
-    leftMotors.move_relative(-1080,50);
-    rightMotors.move_relative(-1080,50);
-    clamp.extend();
-    while(not imu.get_heading()>=90){
-        leftMotors.move(40);
-        rightMotors.move(-40);
-    }
-    leftMotors.brake();
-    rightMotors.brake();
+    chassis.follow(skills3_txt,10,2000,false,false);
+    chassis.follow(skills4_txt,10,2000,true,false);
+    chassis.turnToHeading(150,1000,{},false);
+    chassis.follow(skills5_txt,10,2000,false,false);
+    clamp.retract();
 
 }
 void ClampF(){
@@ -162,11 +175,13 @@ void ClampF(){
         if(controller.get_digital(pros::E_CONTROLLER_DIGITAL_R2) and clampState==0){
             clamp.extend();
             clampState=1;
-            pros::delay(500);
+            pros::delay(200);
         } else if (controller.get_digital(pros::E_CONTROLLER_DIGITAL_R2) and clampState==1){
             clamp.retract();
             clampState=0;
-            pros::delay(500);
+            pros::delay(200);
+        } else{
+            pros::delay(25);
         }
     }
 }
@@ -178,13 +193,14 @@ void ClampF(){
  *z
  * If no competition control is connected, this function will run immediately
  * following initialize().
- * 
  *
  * If the robot is disabled or communications is lost, the
  * operator control task will be stopped. Re-enabling the robot will restart the
  * task, not resume it from where it left off.
  */
 void opcontrol() {
+    chassis.cancelAllMotions();
+    pros::lcd::set_text(2, "Driver Control");
 	pros::Controller master(pros::E_CONTROLLER_MASTER);
     pros::Task ringTask(ring);
     pros::Task clampTask(ClampF);
